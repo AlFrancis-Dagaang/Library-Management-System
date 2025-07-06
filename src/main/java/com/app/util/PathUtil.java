e92346bfd2af328e73b01cf993d7533890443724
Al Francis H. Daga-ang <dagaang.alfrancis@gmail.com>
2025-07-06 12:29:56 +0800

    Added a PathUtil

---
+++ src/main/java/com/app/util/PathUtil.java
@@ -0,0 +1,21 @@
+package com.app.util;
+
+public class PathUtils {
+    public static boolean isNumeric(String str) {
+        if (str == null) return false;
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
+    }
+
+    public static String [] getPaths (String path){
+        if(path != null && !path.isEmpty()){
+            return path.split("/");
+        }else{
+            return null;
+        }
+    }
+}
