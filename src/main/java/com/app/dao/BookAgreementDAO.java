bbb802219fde4ae98a70ca48fc0ef39b2b5593c5
Al Francis H. Daga-ang <dagaang.alfrancis@gmail.com>
2025-07-12 14:14:12 +0800

    Create get book agreement by transaction id

--- src/main/java/com/app/dao/BookAgreementDAO.java
+++ src/main/java/com/app/dao/BookAgreementDAO.java
@@ -14,7 +14,7 @@ public class BookAgreementDAO {
         this.dbConnection = dbConnection;
     }
 
-    public BookAgreement getBookAgreement(int bookAgreementId) {
+    public BookAgreement getBookAgreementById(int bookAgreementId) {
         String sql ="SELECT * FROM book_agreement WHERE agreement_id=?";
 
         try(Connection con = this.dbConnection.getConnection()){
@@ -53,7 +53,7 @@ public class BookAgreementDAO {
             ResultSet rs = ps.getGeneratedKeys();
             if(rs.next()) {
                 int id = rs.getInt(1);
-                return getBookAgreement(id);
+                return getBookAgreementById(id);
             }else{
                 throw new RuntimeException("Create BookAgreement failed");
             }
@@ -75,5 +75,30 @@ public class BookAgreementDAO {
             System.err.println("SQLException: " + e.getMessage());
             throw new RuntimeException("SQLException: " + e.getMessage());
         }
+    }
+
+    public BookAgreement getBookAgreementByTransactionId(int transactionId) {
+        String sql = "SELECT * FROM book_agreement WHERE transaction_id=?";
+
+        try(Connection con = this.dbConnection.getConnection()){
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, transactionId);
+            ResultSet rs = ps.executeQuery();
+            if(rs.next()) {
+                int agreementId = rs.getInt("agreement_id");
+                int transId = rs.getInt("transaction_id");
+                BigDecimal bookPrice = rs.getBigDecimal("book_price");
+                BigDecimal serviceFee = rs.getBigDecimal("service_fee");
+                BigDecimal totalAmount = rs.getBigDecimal("total_amount");
+                LocalDate agreementDate = LocalDate.parse(rs.getString("agreement_date"));
+                boolean active  = rs.getBoolean("active");
+
+                return new BookAgreement(agreementId, transId, bookPrice, serviceFee, totalAmount, agreementDate, active);
+            }
+        }catch (SQLException e){
+            System.err.println("SQLException: " + e.getMessage());
+            throw new RuntimeException("SQLException: " + e.getMessage());
+        }
+        return null;
     }
 }
