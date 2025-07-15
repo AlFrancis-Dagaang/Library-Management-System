21aa998b13ebbc3c44784ecff92801fe13afa890
Al Francis H. Daga-ang <dagaang.alfrancis@gmail.com>
2025-07-16 00:05:27 +0800

    Remove the agreement_id column

--- src/main/java/com/app/dao/BookReturnDAO.java
+++ src/main/java/com/app/dao/BookReturnDAO.java
@@ -10,26 +10,25 @@ import java.time.LocalDate;
 
 public class BookReturnDAO {
     private DBConnection db;
-    public BookReturnDAO(BookAgreementDAO bookAgreementDAO) {
-        db = new DBConnection();
+    public BookReturnDAO(DBConnection db) {
+       this.db = db;
     }
 
 
     public BookReturnStatus createBookReturnStatus(BookReturnStatus bookReturnStatus) {
-        String sql = "INSERT INTO book_return_status (transaction_id, agreement_id, return_type, return_date, book_condition," +
-                "return_timeline, penalty_amount, refund_amount, librarian_notes) VALUES (?,?,?,?,?,?,?,?,?)";
+        String sql = "INSERT INTO book_return_status (transaction_id, return_type, return_date, book_condition," +
+                "return_timeline, penalty_amount, refund_amount, librarian_notes) VALUES (?,?,?,?,?,?,?,?)";
 
         try(Connection con = this.db.getConnection()){
             PreparedStatement ps = con.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS);
             ps.setInt(1, bookReturnStatus.getTransactionId());
-            ps.setInt(2, bookReturnStatus.getAgreementId());
-            ps.setString(3, bookReturnStatus.getReturnType());
-            ps.setDate(4, java.sql.Date.valueOf(bookReturnStatus.getReturnDate()));
-            ps.setString(5, bookReturnStatus.getBookCondition());
-            ps.setString(6, bookReturnStatus.getReturnTimeline());
-            ps.setBigDecimal(7, bookReturnStatus.getPenaltyAmount());
-            ps.setBigDecimal(8, bookReturnStatus.getRefundAmount());
-            ps.setString(9, bookReturnStatus.getLibrarianNotes());
+            ps.setString(2, bookReturnStatus.getReturnType());
+            ps.setDate(3, java.sql.Date.valueOf(bookReturnStatus.getReturnDate()));
+            ps.setString(4, bookReturnStatus.getBookCondition());
+            ps.setString(5, bookReturnStatus.getReturnTimeline());
+            ps.setBigDecimal(6, bookReturnStatus.getPenaltyAmount());
+            ps.setBigDecimal(7, bookReturnStatus.getRefundAmount());
+            ps.setString(8, bookReturnStatus.getLibrarianNotes());
             ps.executeUpdate();
             ResultSet rs = ps.getGeneratedKeys();
             if (rs.next()) {
@@ -53,7 +52,6 @@ public class BookReturnDAO {
             if(rs.next()){
                 int returnStatusId = rs.getInt("return_status_id");
                 int transactionId = rs.getInt("transaction_id");
-                int agreementId = rs.getInt("agreement_id");
                 String returnType = rs.getString("return_type");
                 LocalDate returnDate = LocalDateUtil.getNullableLocalDate(rs, "return_date");
                 String bookCondition = rs.getString("book_condition");
@@ -62,10 +60,8 @@ public class BookReturnDAO {
                 BigDecimal refundAmount = rs.getBigDecimal("refund_amount");
                 String librarianNotes = rs.getString("librarian_notes");
 
-                return new BookReturnStatus(returnStatusId, transactionId, agreementId, returnType, returnDate, bookCondition, returnTimeline, penaltyAmount, refundAmount, librarianNotes);
+                return new BookReturnStatus(returnStatusId, transactionId, returnType, returnDate, bookCondition, returnTimeline, penaltyAmount, refundAmount, librarianNotes);
             }
-
-
         }catch (SQLException e){
             System.err.println("SQLException caught in getBookReturnStatus(): " + e.getMessage());
             throw new RuntimeException("SQLException caught in getBookReturnStatus(): " + e.getMessage());
