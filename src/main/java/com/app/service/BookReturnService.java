991dd7bd3ae58328be4d532741c1a40196ffc31c
Al Francis H. Daga-ang <dagaang.alfrancis@gmail.com>
2025-07-16 00:05:55 +0800

    Add a BookReturnBill DAO

--- src/main/java/com/app/service/BookReturnService.java
+++ src/main/java/com/app/service/BookReturnService.java
@@ -13,23 +13,30 @@ public class BookReturnService {
     private BookDAO bookDAO;
     private BookTransactionDAO bookTransactionDAO;
     private BookAgreementDAO bookAgreementDAO;
+    private BookReturnBillDAO bookReturnBillDAO;
 
-    public BookReturnService(BookReturnDAO bookReturnDAO, BookDAO bookDAO, BookTransactionDAO bookTransactionDAO, BookAgreementDAO bookAgreementDAO) {
+    public BookReturnService(BookReturnDAO bookReturnDAO, BookDAO bookDAO, BookTransactionDAO bookTransactionDAO, BookAgreementDAO bookAgreementDAO, BookReturnBillDAO bookReturnBillDAO) {
         this.bookReturnDAO = bookReturnDAO;
         this.bookDAO = bookDAO;
         this.bookTransactionDAO = bookTransactionDAO;
         this.bookAgreementDAO = bookAgreementDAO;
+        this.bookReturnBillDAO = bookReturnBillDAO;
     }
 
-    public BookReturnBill createBookReturn(BookReturnStatus bookReturn) {
+    public Object processBookReturn(BookReturnStatus bookReturn) {
         BookTransaction bookTransaction = this.bookTransactionDAO.getIssueTransactionById(bookReturn.getTransactionId());
         Book book = this.bookDAO.getBookById(bookTransaction.getBookId());
+
+        BookAgreement bookAgreement = this.bookAgreementDAO.getBookAgreementByTransactionId(bookTransaction.getIssueId());
 
         LocalDate returnDate = LocalDate.now();
         bookReturn.setReturnDate(returnDate);
 
         bookTransaction.setReturnDate(returnDate); //update return date for BookTransaction
 
+        bookTransaction.setStatus("RETURNED");// Always RETURNED otherwise if the overdue is true
+
+        //Determine if the return is On time or Overdue
         if(bookTransaction.getDueDate().isBefore(returnDate)) {
             bookReturn.setReturnTimeline("Overdue");
         }else{
@@ -42,25 +49,54 @@ public class BookReturnService {
         String returnType = bookReturn.getReturnType();
         String timeLine = bookReturn.getReturnTimeline();
 
+        //Calculating penalty amount base on the condition and timeline of return
         BigDecimal penaltyBaseOnCondition= BookBankUtil.calculatePenaltyBaseOnCondition(conditionOfBook, returnType, book.getPrice());
         BigDecimal penaltyBaseOnTimeline = BookBankUtil.calculatePenaltyBaseOnReturnTimeline(timeLine, returnType );
         BigDecimal penaltyAmount = penaltyBaseOnCondition.add(penaltyBaseOnTimeline);
         bookReturn.setPenaltyAmount(penaltyAmount);
 
+
         book.setAvailable(true);//update book availability to available
 
-
-        if("BOOK)_BANK".equals(returnType)) {
-            BigDecimal refundAmount = book.getPrice().subtract(penaltyAmount);
-            bookReturn.setRefundAmount(refundAmount);
-            BookReturnStatus createdReturnStatus = this.bookReturnDAO.createBookReturnStatus(bookReturn);
-
-            BookReturnBill returnBill = new BookReturnBill(createdReturnStatus.getReturnStatusId(), penaltyAmount, refundAmount, "PENDING", returnDate );
-
-
+        if(bookAgreement!=null) {
+            bookAgreement.setActive(false); // update the agreement false
         }
 
-        return null;
+        if("BOOK_BANK".equals(returnType) && bookAgreement!=null) {
+            BigDecimal totalAmount = bookAgreement.getTotalAmount();
+            BigDecimal refundAmount = book.getPrice().subtract(penaltyAmount);
+            bookReturn.setRefundAmount(refundAmount);
+
+            //Creating BookReturnStatus
+            BookReturnStatus createdReturnStatus = this.bookReturnDAO.createBookReturnStatus(bookReturn);
+
+            //Update method in DAO
+            this.bookDAO.updateBook(book, book.getBookId());//Update the book
+            this.bookTransactionDAO.updateBookTransaction(bookTransaction, bookTransaction.getIssueId());
+            this.bookAgreementDAO.updateBookAgreement(bookAgreement, bookAgreement.getAgreementId());
+
+            BookReturnBill returnBill = new BookReturnBill(createdReturnStatus.getReturnStatusId(),totalAmount, penaltyAmount, refundAmount, "PENDING", returnDate );
+
+            return this.bookReturnBillDAO.createBookReturnBill(returnBill);
+
+        }else if("GENERAL".equals(returnType)) {
+            //Creating BookReturnStatus
+            BookReturnStatus createdReturnStatus = this.bookReturnDAO.createBookReturnStatus(bookReturn);
+
+            //Update method in DAO
+            this.bookDAO.updateBook(book, book.getBookId());
+            this.bookTransactionDAO.updateBookTransaction(bookTransaction, bookTransaction.getIssueId());
+
+            //If GENERAL BOOK is either not in good condition or not return on time
+            if(!bookReturn.getBookCondition().equals("Good") || !bookReturn.getReturnTimeline().equals("On Time")) {
+                //Creating BookReturnBill
+                BookReturnBill returnBill = new BookReturnBill(createdReturnStatus.getReturnStatusId(), penaltyAmount,"PENDING", returnDate );
+                return this.bookReturnBillDAO.createBookReturnBill(returnBill);
+            }
+            return createdReturnStatus;
+        }else{
+            throw new RuntimeException("Book type is not identified when returning a book");
+        }
     }
 }
 
