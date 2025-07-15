e8af449bb21a46e7a76cc250339be739621c08d4
Al Francis H. Daga-ang <dagaang.alfrancis@gmail.com>
2025-07-16 00:08:46 +0800

    Add BookReturn Service and Fix the JsonUtil Reader once bug

--- src/main/java/com/app/controller/BookTransactionServlet.java
+++ src/main/java/com/app/controller/BookTransactionServlet.java
@@ -2,10 +2,8 @@ package com.app.controller;
 
 import com.app.config.AppConfig;
 import com.app.exception.ResourceNotFound;
-import com.app.model.BookAgreement;
-import com.app.model.BookTransaction;
-import com.app.model.BookTransactionAgreementDetailsDTO;
-import com.app.model.BookTransactionDetailsDTO;
+import com.app.model.*;
+import com.app.service.BookReturnService;
 import com.app.service.BookTransactionService;
 import com.app.util.JsonUtil;
 import com.app.util.PathUtil;
@@ -15,6 +13,7 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.util.List;
 
@@ -22,21 +21,24 @@ import java.util.List;
 public class BookTransactionServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
     private BookTransactionService bookTransactionService;
+    private BookReturnService bookReturnService;
 
     @Override
     public void init(){
+
         this.bookTransactionService = AppConfig.getBookIssueService();
+        this.bookReturnService = AppConfig.getBookReturnService();
     }
 
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String path = request.getPathInfo();
         String [] paths = PathUtil.getPaths(path);
-        BookTransaction transaction = JsonUtil.parse(request, BookTransaction.class);
 
-        System.out.println(transaction);
+
 
         try{
             if(path == null || path.isEmpty()){ // "Create Transaction for both General and Book Bank"
+                BookTransaction transaction = JsonUtil.parse(request, BookTransaction.class);
                 Object transactionalObject = this.bookTransactionService.createTransaction(transaction);
                 JsonUtil.writeOk(response, HttpServletResponse.SC_CREATED, "", transactionalObject);
             }else if(paths.length==3 && PathUtil.isNumeric(paths[1]) && paths[2].equals("complete-transaction")){ // /{id}/complete-transaction
@@ -47,6 +49,10 @@ public class BookTransactionServlet extends HttpServlet {
                 int transactionId = Integer.parseInt(paths[1]);
                 BookTransaction bookCancelledTransaction = this.bookTransactionService.cancelBookAgreement(transactionId);
                 JsonUtil.writeOk(response, HttpServletResponse.SC_OK,"Success", bookCancelledTransaction);
+            } else if(paths.length==2 && paths[1].equals("return")) { // /return
+                BookReturnStatus returnBook = JsonUtil.parse(request, BookReturnStatus.class);
+                Object bookReturned = this.bookReturnService.processBookReturn(returnBook);
+                JsonUtil.writeOk(response, HttpServletResponse.SC_OK,"Success", bookReturned);
             }
         }catch (IllegalArgumentException e){
             JsonUtil.writeError(response, HttpServletResponse.SC_BAD_REQUEST, e.getMessage());
